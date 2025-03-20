ebda74e8547a55ae1fd08bc8ba41fe22d9ebfbdc
Khachatryan Karen <karen0734@gmail.com>
2025-03-20 20:20:56 +0300

    Fix log

--- src/dns-server-test.c
+++ src/dns-server-test.c
@@ -1,9 +1,9 @@
 #include "dns-server-test.h"
 
-int32_t sended;
-int32_t readed;
+volatile int32_t sended;
+volatile int32_t readed;
 
-int32_t error_count;
+volatile int32_t error_count;
 
 array_hashmap_t domains_map_struct;
 
@@ -115,20 +115,46 @@ int32_t get_domain_from_packet(memory_t *receive_msg, char *cur_pos_ptr, char **
 
 void *stat(__attribute__((unused)) void *arg)
 {
-    printf("Send_RPS Read_RPS Sended Readed Diff Errors\n");
-
     int32_t sended_old = 0;
     int32_t readed_old = 0;
+
+    time_t now = time(NULL);
+    struct tm *tm_struct = localtime(&now);
+    printf("\nStart time %02d.%02d.%04d %02d:%02d:%02d\n\n", tm_struct->tm_mday,
+           tm_struct->tm_mon + 1, tm_struct->tm_year + 1900, tm_struct->tm_hour, tm_struct->tm_min,
+           tm_struct->tm_sec);
+
+    char print_format[100];
+    char *print_data[100];
+    print_data[0] = "Send_RPS;";
+    print_data[1] = " Read_RPS;";
+    print_data[2] = "   Sended;";
+    print_data[3] = "   Readed;";
+    print_data[4] = "     Diff;";
+    print_data[5] = "   Errors;";
+
+    for (int32_t i = 0; i < 6; i++) {
+        printf("%s", print_data[i]);
+    }
+    printf("\n");
 
     while (true) {
         sleep(1);
 
-        time_t now = time(NULL);
-        struct tm *tm_struct = localtime(&now);
-        printf("\n%02d.%02d.%04d %02d:%02d:%02d\n", tm_struct->tm_mday, tm_struct->tm_mon + 1,
-               tm_struct->tm_year + 1900, tm_struct->tm_hour, tm_struct->tm_min, tm_struct->tm_sec);
-        printf("%8d %8d %6d %6d %4d %6d\n", sended - sended_old, readed - readed_old, sended,
-               readed, readed - sended, error_count);
+        sprintf(print_format, "%%%dd;", (int32_t)(strlen(print_data[0]) - 1));
+        printf(print_format, sended - sended_old);
+        sprintf(print_format, "%%%dd;", (int32_t)(strlen(print_data[1]) - 1));
+        printf(print_format, readed - readed_old);
+        sprintf(print_format, "%%%dd;", (int32_t)(strlen(print_data[2]) - 1));
+        printf(print_format, sended);
+        sprintf(print_format, "%%%dd;", (int32_t)(strlen(print_data[3]) - 1));
+        printf(print_format, readed);
+        sprintf(print_format, "%%%dd;", (int32_t)(strlen(print_data[4]) - 1));
+        printf(print_format, readed - sended);
+        sprintf(print_format, "%%%dd;", (int32_t)(strlen(print_data[5]) - 1));
+        printf(print_format, error_count);
+        printf("\n");
+        fflush(stdout);
 
         sended_old = sended;
         readed_old = readed;
@@ -170,133 +196,178 @@ void print_help(void)
            "    -l  \"x.x.x.x:xx\"  Listen address\n");
 }
 
+static void main_catch_function(int32_t signo)
+{
+    if (signo == SIGINT) {
+        errmsg("SIGINT catched main\n");
+    } else if (signo == SIGSEGV) {
+        errmsg("SIGSEGV catched main\n");
+    } else if (signo == SIGTERM) {
+        errmsg("SIGTERM catched main\n");
+    }
+}
+
 int32_t main(int32_t argc, char *argv[])
 {
-    FILE *cache_fp = NULL;
+    printf("DNS server test started\n\n");
 
-    struct sockaddr_in listen_addr, client_addr;
-    int32_t listen_socket;
+    if (signal(SIGINT, main_catch_function) == SIG_ERR) {
+        errmsg("Can't set SIGINT signal handler main\n");
+    }
 
-    uint32_t client_addr_length = sizeof(client_addr);
+    if (signal(SIGSEGV, main_catch_function) == SIG_ERR) {
+        errmsg("Can't set SIGSEGV signal handler main\n");
+    }
 
-    printf("DNS server test started\n");
-    printf("Launch parameters:\n");
+    if (signal(SIGTERM, main_catch_function) == SIG_ERR) {
+        errmsg("Can't set SIGTERM signal handler main\n");
+    }
 
+    struct sockaddr_in listen_addr;
     listen_addr.sin_addr.s_addr = INADDR_NONE;
 
-    for (int32_t i = 1; i < argc; i++) {
-        if (!strcmp(argv[i], "-l")) {
-            if (i != argc - 1) {
-                printf("  Listen  \"%s\"\n", argv[i + 1]);
-                char *colon_ptr = strchr(argv[i + 1], ':');
-                if (colon_ptr) {
-                    uint16_t tmp_port = 0;
-                    sscanf(colon_ptr + 1, "%hu", &tmp_port);
-                    *colon_ptr = 0;
-                    if (strlen(argv[i + 1]) < INET_ADDRSTRLEN) {
-                        listen_addr.sin_family = AF_INET;
-                        listen_addr.sin_port = htons(tmp_port);
-                        listen_addr.sin_addr.s_addr = inet_addr(argv[i + 1]);
+    //Args
+    {
+        printf("Launch parameters:\n");
+        for (int32_t i = 1; i < argc; i++) {
+            if (!strcmp(argv[i], "-l")) {
+                if (i != argc - 1) {
+                    printf("  Listen  \"%s\"\n", argv[i + 1]);
+                    char *colon_ptr = strchr(argv[i + 1], ':');
+                    if (colon_ptr) {
+                        uint16_t tmp_port = 0;
+                        sscanf(colon_ptr + 1, "%hu", &tmp_port);
+                        *colon_ptr = 0;
+                        if (strlen(argv[i + 1]) < INET_ADDRSTRLEN) {
+                            listen_addr.sin_family = AF_INET;
+                            listen_addr.sin_port = htons(tmp_port);
+                            listen_addr.sin_addr.s_addr = inet_addr(argv[i + 1]);
+                        }
+                        *colon_ptr = ':';
                     }
-                    *colon_ptr = ':';
+                    i++;
                 }
-                i++;
+                continue;
             }
-            continue;
+            print_help();
+            errmsg("Unknown command %s\n", argv[i]);
         }
-        print_help();
-        errmsg("Unknown command %s\n", argv[i]);
+
+        if (listen_addr.sin_addr.s_addr == INADDR_NONE) {
+            print_help();
+            errmsg("The program need correct listen IP\n");
+        }
+
+        if (listen_addr.sin_port == 0) {
+            print_help();
+            errmsg("The program need correct listen port\n");
+        }
     }
+    //Args
 
-    if (listen_addr.sin_addr.s_addr == INADDR_NONE) {
-        print_help();
-        errmsg("The program need correct listen IP\n");
+    //Read cache.data
+    {
+        FILE *cache_fp = NULL;
+        cache_fp = fopen("cache.data", "r");
+        if (!cache_fp) {
+            errmsg("Can't open file cache.data\n");
+        }
+
+        memory_t cache_data;
+
+        fseek(cache_fp, 0, SEEK_END);
+        cache_data.size = ftell(cache_fp);
+        fseek(cache_fp, 0, SEEK_SET);
+
+        cache_data.data = (char *)malloc(cache_data.size);
+
+        if (fread(cache_data.data, sizeof(char), cache_data.size, cache_fp) !=
+            (size_t)cache_data.size) {
+            errmsg("Can't read cache.data file\n");
+        }
+
+        domains_map_struct = array_hashmap_init(1000000, 1.0, sizeof(domain_data_t));
+        if (domains_map_struct == NULL) {
+            errmsg("No free memory for domains_map_struct\n");
+        }
+
+        array_hashmap_set_func(domains_map_struct, domain_add_hash, domain_add_cmp,
+                               domain_find_hash, domain_find_cmp, domain_find_hash,
+                               domain_find_cmp);
+
+        char *cur_pos_ptr = cache_data.data;
+        char *cache_data_end = cache_data.data + cache_data.size;
+
+        while (cur_pos_ptr < cache_data_end) {
+            char *domain_data = cur_pos_ptr;
+            int32_t domain_len = strlen(cur_pos_ptr);
+
+            cur_pos_ptr += domain_len + 1;
+            int32_t *packet_size = (int32_t *)cur_pos_ptr;
+
+            domain_data_t add_elem;
+            add_elem.packet = cur_pos_ptr + sizeof(int32_t);
+            add_elem.packet_size = *packet_size;
+            add_elem.domain = domain_data;
+
+            array_hashmap_add_elem(domains_map_struct, &add_elem, NULL, NULL);
+
+            cur_pos_ptr += *packet_size + sizeof(int32_t);
+        }
     }
+    //Read cache.data
 
-    if (listen_addr.sin_port == 0) {
-        print_help();
-        errmsg("The program need correct listen port\n");
+    int32_t listen_socket;
+
+    //Open socket
+    {
+        listen_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        if (listen_socket < 0) {
+            errmsg("Can't create socket %s\n", strerror(errno));
+        }
+
+        if (bind(listen_socket, (struct sockaddr *)&listen_addr, sizeof(listen_addr)) < 0) {
+            errmsg("Can't bind to the port %s\n", strerror(errno));
+        }
     }
-
-    cache_fp = fopen("cache.data", "r");
-    if (!cache_fp) {
-        errmsg("Can't open file cache.data\n");
-    }
-
-    memory_t cache_data;
-
-    fseek(cache_fp, 0, SEEK_END);
-    cache_data.size = ftell(cache_fp);
-    fseek(cache_fp, 0, SEEK_SET);
-
-    cache_data.data = (char *)malloc(cache_data.size);
-
-    if (fread(cache_data.data, sizeof(char), cache_data.size, cache_fp) !=
-        (size_t)cache_data.size) {
-        errmsg("Can't read cache.data file\n");
-    }
-
-    domains_map_struct = array_hashmap_init(1000000, 1.0, sizeof(domain_data_t));
-    if (domains_map_struct == NULL) {
-        errmsg("No free memory for domains_map_struct\n");
-    }
-
-    array_hashmap_set_func(domains_map_struct, domain_add_hash, domain_add_cmp, domain_find_hash,
-                           domain_find_cmp, domain_find_hash, domain_find_cmp);
-
-    char *cur_pos_ptr = cache_data.data;
-    char *cache_data_end = cache_data.data + cache_data.size;
-
-    while (cur_pos_ptr < cache_data_end) {
-        char *domain_data = cur_pos_ptr;
-        int32_t domain_len = strlen(cur_pos_ptr);
-
-        cur_pos_ptr += domain_len + 1;
-        int32_t *packet_size = (int32_t *)cur_pos_ptr;
-
-        domain_data_t add_elem;
-        add_elem.packet = cur_pos_ptr + sizeof(int32_t);
-        add_elem.packet_size = *packet_size;
-        add_elem.domain = domain_data;
-
-        array_hashmap_add_elem(domains_map_struct, &add_elem, NULL, NULL);
-
-        cur_pos_ptr += *packet_size + sizeof(int32_t);
-    }
-
-    listen_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (listen_socket < 0) {
-        errmsg("Can't create socket %s\n", strerror(errno));
-    }
-
-    if (bind(listen_socket, (struct sockaddr *)&listen_addr, sizeof(listen_addr)) < 0) {
-        errmsg("Can't bind to the port %s\n", strerror(errno));
-    }
+    //Open socket
 
     memory_t receive_msg;
-    receive_msg.size = 0;
-    receive_msg.max_size = PACKET_MAX_SIZE;
-    receive_msg.data = (char *)malloc(receive_msg.max_size * sizeof(char));
-    if (receive_msg.data == 0) {
-        errmsg("No free memory for receive_msg from client\n");
-    }
-
     memory_t que_domain;
-    que_domain.size = 0;
-    que_domain.max_size = DOMAIN_MAX_SIZE;
-    que_domain.data = (char *)malloc(que_domain.max_size * sizeof(char));
-    if (que_domain.data == 0) {
-        errmsg("No free memory for que_domain\n");
-    }
 
-    pthread_t stat_thread;
-    if (pthread_create(&stat_thread, NULL, stat, NULL)) {
-        errmsg("Can't create stat_thread\n");
-    }
+    //Alloc
+    {
+        receive_msg.size = 0;
+        receive_msg.max_size = PACKET_MAX_SIZE;
+        receive_msg.data = (char *)malloc(receive_msg.max_size * sizeof(char));
+        if (receive_msg.data == 0) {
+            errmsg("No free memory for receive_msg from client\n");
+        }
 
-    if (pthread_detach(stat_thread)) {
-        errmsg("Can't detach stat_thread\n");
+        que_domain.size = 0;
+        que_domain.max_size = DOMAIN_MAX_SIZE;
+        que_domain.data = (char *)malloc(que_domain.max_size * sizeof(char));
+        if (que_domain.data == 0) {
+            errmsg("No free memory for que_domain\n");
+        }
     }
+    //Alloc
+
+    struct sockaddr_in client_addr;
+    uint32_t client_addr_length = sizeof(client_addr);
+
+    //Threads
+    {
+        pthread_t stat_thread;
+        if (pthread_create(&stat_thread, NULL, stat, NULL)) {
+            errmsg("Can't create stat_thread\n");
+        }
+
+        if (pthread_detach(stat_thread)) {
+            errmsg("Can't detach stat_thread\n");
+        }
+    }
+    //Threads
 
     while (true) {
         receive_msg.size = recvfrom(listen_socket, receive_msg.data, receive_msg.max_size, 0,
